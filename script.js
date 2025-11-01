18fba8de041fc1cbc0f5552d386e90231b5c965e
Makar Morgun <morgunmakar@gmail.com>
2025-11-01 23:11:02 +0300

    comment

--- script.js
+++ script.js
@@ -1,30 +1,47 @@
-let storageTask = localStorage.getItem('storageTask')
+let storageTask = localStorage.getItem('storageTask');
+if (!storageTask) {
+    storageTask = {}; 
+} else {
+    storageTask = JSON.parse(storageTask);
+}
 
 function createTask(){
     let newTaskText = document.querySelector('.newTaskText')
-    let idTask = 1
+    console.log(newTaskText)
 
-    storageTask.idTask = {
+    if (!newTaskText.value.trim()) {
+        alert('Введите текст задачи!');
+        return;
+    }
+
+    let idTask = Object.keys(storageTask).length + 1;
+
+
+    storageTask[idTask] = {
         id: idTask,
         state: 'doing',
-        time: new Date(),
-        text: newTaskText.textContent,
-    }
+        time: new Date().toLocaleString(), 
+        text: newTaskText.value,
+    };
+
     localStorage.setItem('storageTask',storageTask)
-    newTaskText.textContent = ''
+    newTaskText.value = '';
+    updateTaskList();
 }
 
 function updateTaskList(){
-    storageTask.forEach(task => {
-        let newTaskText = task.text
-        let newTaskId = task.id
-        let newTime = task.time
-        let newState = task.state
+    console.log('123')
+    
+    document.querySelector('.taskList').innerHTML = '';
+    
+    let tasks = Object.values(storageTask);
 
-        let newTaskContainer = `<div class="taskСontent" class='${newState}' id='taslId${newTaskId}'>
+    tasks.forEach(task => {
+    
+        let newTaskContainer = `<div class="taskСontent" class='${task.state}' id='taslId${task.id}'>
                                     <input type = 'checkbox' class="checktask">
-                                    <h2 class="h2">${newTaskText}</h3>
-                                    <p class="timeData">${newTime}</p>
+                                    <h2 class="h2">${task.text}</h3>
+                                    <p class="timeData">${task.time}</p>
                                     <button class="del">
                                         <img src="./pictures/trash.png" alt="">
                                     </button>
@@ -37,6 +54,24 @@ function updateTaskList(){
     })
 }
 
+document.querySelector('.addTaskBtn').addEventListener('click', () => {
+    document.querySelector('.modalBackground').style.display = 'block';
+    document.querySelector('.modal').style.display = 'block';
+});
 
+document.querySelector('.modalBackground').addEventListener('click', () => {
+    document.querySelector('.modalBackground').style.display = 'none';
+    document.querySelector('.modal').style.display = 'none';
+});
+
+document.querySelector('.taskList').addEventListener('change', (event) => {
+    if (event.target.classList.contains('checktask')) {
+        let taskId = event.target.closest('.taskСontent').id.replace('taskId', '');
+        let task = storageTask[taskId];
+        task.state = event.target.checked ? 'completed' : 'doing';
+        localStorage.setItem('storageTask', JSON.stringify(storageTask));
+        updateTaskList();
+    }
+});
 
 updateTaskList();
