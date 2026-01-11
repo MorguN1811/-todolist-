757f929359d26245ab106a5fce7338e6194de7d2
Makar Morgun <morgunmakar@gmail.com>
2026-01-11 22:30:12 +0300

    comment

--- script.js
+++ script.js
@@ -28,6 +28,9 @@ function createTask(){
         text: newTaskText.value,
     };
 
+    document.querySelector('modalBackground').style.display = 'none'
+    document.querySelector('.modal').style.display = 'none'
+
     localStorage.setItem('storageTask',storageTask)
     newTaskText.value = '';
     updateTaskList();
@@ -53,6 +56,20 @@ document.querySelector('.taskList').addEventListener('change', (event) => {
     }
 });
 
+function deleteTask(taskId){
+    delete storageTask[taskId];
+    localStorage.setItem('storageTask',JSON.stringify(storageTask))
+    updateTaskList();
+}
+
+function editTask(taskId){
+    let task = storageTask[taskId];
+    task.text = newText;
+    localStorage.setItem('storageTask',JSON.stringify(storageTask))
+    updateTaskList();
+}
+
+
 function updateTaskList(){
     console.log('123')
     
@@ -66,10 +83,10 @@ function updateTaskList(){
                                     <input type = 'checkbox' class="checktask">
                                     <h2 class="/h2">${task.text}</h3>
                                     <p class="timeData">${task.time}</p>
-                                    <button class="del">
+                                    <button class="del"> onclik="editTask(${task.id}"   
                                         <img src="./pictures/trash.png" alt="">
                                     </button>
-                                    <button class="edit">
+                                    <button class="edit"> onclik="editTask(${task.id},promt('Введите новый текст:','${task.text}'))"
                                         <img src="./pictures/pencil.png" alt="">
                                     </button>
                                 </div>`
@@ -77,5 +94,6 @@ function updateTaskList(){
         document.querySelector('.taskList').innerHTML += newTaskContainer;
     })
 }
+
 
 updateTaskList();
